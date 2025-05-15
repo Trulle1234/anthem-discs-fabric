c2b6cf4137cfbde45fdff19471ce7fb163be80bf
Trulle1234 <leonard.trulsson@gmail.com>
2025-05-15 19:49:21 +0200

    added first item

--- src/main/java/com/trulle123/anthemdiscs/item/ModItems.java
+++ src/main/java/com/trulle123/anthemdiscs/item/ModItems.java
@@ -1,27 +1,32 @@
 package com.trulle123.anthemdiscs.item;
 
 import com.trulle123.anthemdiscs.AnthemDiscs;
-
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    public static final Item MUSIC_DISC_EMPTY = registerItem("music_disc_empty", new Item(new Item.Settings()));
+    public static final Item MUSIC_DISC_EMPTY = registerItem(
+        "music_disc_empty",
+        new Item.Settings()
+    );
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(AnthemDiscs.MOD_ID, name),item );
+    private static Item registerItem(String name, Item.Settings settings) {
+        Identifier id = Identifier.of(AnthemDiscs.MOD_ID, name);
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+        Item.Settings keyedSettings = settings.registryKey(key);
+        Item item = new Item(keyedSettings);
+        return Registry.register(Registries.ITEM, key, item);
     }
-
 
     public static void registerModItems() {
         AnthemDiscs.LOGGER.info("Registering Mod Items for " + AnthemDiscs.MOD_ID);
-
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-            entries.add(MUSIC_DISC_EMPTY);
-        });
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS)
+                       .register(entries -> entries.add(MUSIC_DISC_EMPTY));
     }
 }
