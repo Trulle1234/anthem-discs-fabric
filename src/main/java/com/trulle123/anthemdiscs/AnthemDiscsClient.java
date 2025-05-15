dc551219780875903d51d7e08c9bb80d07c0e815
Trulle1234 <leonard.trulsson@gmail.com>
2025-05-15 16:49:06 +0200

    setup

---
+++ src/main/java/com/trulle123/anthemdiscs/AnthemDiscsClient.java
@@ -0,0 +1,13 @@
+package com.trulle123.anthemdiscs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnthemDiscsClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onInitializeClient'");
+    }
+    
+}
