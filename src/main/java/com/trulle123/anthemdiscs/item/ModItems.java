96c685b99e482f4a9414ac0c0abc32a0e1a08c42
Trulle1234 <leonard.trulsson@gmail.com>
2025-05-25 16:55:28 +0200

    I did some thing, but nothing works a single shit

--- src/main/java/com/trulle123/anthemdiscs/item/ModItems.java
+++ src/main/java/com/trulle123/anthemdiscs/item/ModItems.java
@@ -18,10 +18,354 @@ public class ModItems {
         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
     );
 
-        public static final Item MUSIC_DISC_ALBANIA = registerItem(
+
+    public static final Item MUSIC_DISC_ALBANIA = registerItem(
         "music_disc_albania",
-        new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
-        .jukeboxPlayable(ModSounds.ALBANIA_KEY).maxCount(1)
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.ALBANIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_ANDORRA = registerItem(
+        "music_disc_andorra",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.ANDORRA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_ARMENIA = registerItem(
+        "music_disc_armenia",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.ARMENIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_AUSTRIA = registerItem(
+        "music_disc_austria",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.AUSTRIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_AZERBAIJAN = registerItem(
+        "music_disc_azerbaijan",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.AZERBAIJAN_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_BELARUS = registerItem(
+        "music_disc_belarus",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.BELARUS_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_BELGIUM = registerItem(
+        "music_disc_belgium",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.BELGIUM_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_BOSNIA = registerItem(
+        "music_disc_bosnia",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.BOSNIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_BULGARIA = registerItem(
+        "music_disc_bulgaria",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.BULGARIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_CROATIA = registerItem(
+        "music_disc_croatia",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.CROATIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_CZECHIA = registerItem(
+        "music_disc_czechia",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.CZECHIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_DENMARK = registerItem(
+        "music_disc_denmark",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.DENMARK_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_FINLAND = registerItem(
+        "music_disc_finland",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.FINLAND_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_FRANCE = registerItem(
+        "music_disc_france",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.FRANCE_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_GEORGIA = registerItem(
+        "music_disc_georgia",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.GEORGIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_GERMANY = registerItem(
+        "music_disc_germany",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.GERMANY_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_GIBRALTAR = registerItem(
+        "music_disc_gibraltar",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.GIBRALTAR_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_GREECE = registerItem(
+        "music_disc_greece",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.GREECE_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_GUERNSEY = registerItem(
+        "music_disc_guernsey",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.GUERNSEY_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_HUNGARY = registerItem(
+        "music_disc_hungary",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.HUNGARY_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_ICELAND = registerItem(
+        "music_disc_iceland",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.ICELAND_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_IRELAND = registerItem(
+        "music_disc_ireland",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.IRELAND_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_ISLE_OF_MAN = registerItem(
+        "music_disc_isle_of_man",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.ISLE_OF_MAN_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_ITALY = registerItem(
+        "music_disc_italy",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.ITALY_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_JERSEY = registerItem(
+        "music_disc_jersey",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.JERSEY_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_LATVIA = registerItem(
+        "music_disc_latvia",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.LATVIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_LITHUANIA = registerItem(
+        "music_disc_lithuania",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.LITHUANIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_LUXEMBOURG = registerItem(
+        "music_disc_luxembourg",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.LUXEMBOURG_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_MACEDONIA = registerItem(
+        "music_disc_macedonia",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.MACEDONIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_MALTA = registerItem(
+        "music_disc_malta",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.MALTA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_MOLDOVA = registerItem(
+        "music_disc_moldova",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.MOLDOVA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_MONACO = registerItem(
+        "music_disc_monaco",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.MONACO_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_MONTENEGRO = registerItem(
+        "music_disc_montenegro",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.MONTENEGRO_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_NETHERLANDS = registerItem(
+        "music_disc_netherlands",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.NETHERLANDS_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_NORWAY = registerItem(
+        "music_disc_norway",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.NORWAY_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_POLAND = registerItem(
+        "music_disc_poland",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.POLAND_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_PORTUGAL = registerItem(
+        "music_disc_portugal",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.PORTUGAL_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_ROMANIA = registerItem(
+        "music_disc_romania",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.ROMANIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_RUSSIA = registerItem(
+        "music_disc_russia",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.RUSSIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_SAN_MARINO = registerItem(
+        "music_disc_san_marino",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.SAN_MARINO_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_SERBIA = registerItem(
+        "music_disc_serbia",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.SERBIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_SLOVAKIA = registerItem(
+        "music_disc_slovakia",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.SLOVAKIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_SLOVENIA = registerItem(
+        "music_disc_slovenia",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.SLOVENIA_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_SPAIN = registerItem(
+        "music_disc_spain",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.SPAIN_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_SWEDEN = registerItem(
+        "music_disc_sweden",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.SWEDEN_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_SWITZERLAND = registerItem(
+        "music_disc_switzerland",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.SWITZERLAND_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_TURKEY = registerItem(
+        "music_disc_turkey",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.TURKEY_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_UK = registerItem(
+        "music_disc_uk",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.UK_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_UKRAINE = registerItem(
+        "music_disc_ukraine",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.UKRAINE_KEY).maxCount(1)
+    );
+
+
+    public static final Item MUSIC_DISC_VATICAN = registerItem(
+        "music_disc_vatican",
+         new Item.Settings().rarity(net.minecraft.util.Rarity.RARE)
+            .jukeboxPlayable(ModSounds.VATICAN_KEY).maxCount(1)
     );
 
     private static Item registerItem(String name, Item.Settings settings) {
--- src/main/java/com/trulle123/anthemdiscs/sound/ModSounds.java
+++ src/main/java/com/trulle123/anthemdiscs/sound/ModSounds.java
@@ -14,9 +14,256 @@ public class ModSounds {
 
     public static final SoundEvent I_AM_STEVE = registerSoundEvent("i_am_steve");
 
+
     public static final SoundEvent ALBANIA = registerSoundEvent("albania");
-    public static final RegistryKey<JukeboxSong> ALBANIA_KEY = 
+    public static final RegistryKey<JukeboxSong> ALBANIA_KEY =
         RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "albania"));
+
+
+    public static final SoundEvent ANDORRA = registerSoundEvent("andorra");
+    public static final RegistryKey<JukeboxSong> ANDORRA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "andorra"));
+
+
+    public static final SoundEvent ARMENIA = registerSoundEvent("armenia");
+    public static final RegistryKey<JukeboxSong> ARMENIA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "armenia"));
+
+
+    public static final SoundEvent AUSTRIA = registerSoundEvent("austria");
+    public static final RegistryKey<JukeboxSong> AUSTRIA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "austria"));
+
+
+    public static final SoundEvent AZERBAIJAN = registerSoundEvent("azerbaijan");
+    public static final RegistryKey<JukeboxSong> AZERBAIJAN_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "azerbaijan"));
+
+
+    public static final SoundEvent BELARUS = registerSoundEvent("belarus");
+    public static final RegistryKey<JukeboxSong> BELARUS_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "belarus"));
+
+
+    public static final SoundEvent BELGIUM = registerSoundEvent("belgium");
+    public static final RegistryKey<JukeboxSong> BELGIUM_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "belgium"));
+
+
+    public static final SoundEvent BOSNIA = registerSoundEvent("bosnia");
+    public static final RegistryKey<JukeboxSong> BOSNIA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "bosnia"));
+
+
+    public static final SoundEvent BULGARIA = registerSoundEvent("bulgaria");
+    public static final RegistryKey<JukeboxSong> BULGARIA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "bulgaria"));
+
+
+    public static final SoundEvent CROATIA = registerSoundEvent("croatia");
+    public static final RegistryKey<JukeboxSong> CROATIA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "croatia"));
+
+
+    public static final SoundEvent CZECHIA = registerSoundEvent("czechia");
+    public static final RegistryKey<JukeboxSong> CZECHIA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "czechia"));
+
+
+    public static final SoundEvent DENMARK = registerSoundEvent("denmark");
+    public static final RegistryKey<JukeboxSong> DENMARK_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "denmark"));
+
+
+    public static final SoundEvent FINLAND = registerSoundEvent("finland");
+    public static final RegistryKey<JukeboxSong> FINLAND_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "finland"));
+
+
+    public static final SoundEvent FRANCE = registerSoundEvent("france");
+    public static final RegistryKey<JukeboxSong> FRANCE_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "france"));
+
+
+    public static final SoundEvent GEORGIA = registerSoundEvent("georgia");
+    public static final RegistryKey<JukeboxSong> GEORGIA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "georgia"));
+
+
+    public static final SoundEvent GERMANY = registerSoundEvent("germany");
+    public static final RegistryKey<JukeboxSong> GERMANY_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "germany"));
+
+
+    public static final SoundEvent GIBRALTAR = registerSoundEvent("gibraltar");
+    public static final RegistryKey<JukeboxSong> GIBRALTAR_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "gibraltar"));
+
+
+    public static final SoundEvent GREECE = registerSoundEvent("greece");
+    public static final RegistryKey<JukeboxSong> GREECE_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "greece"));
+
+
+    public static final SoundEvent GUERNSEY = registerSoundEvent("guernsey");
+    public static final RegistryKey<JukeboxSong> GUERNSEY_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "guernsey"));
+
+
+    public static final SoundEvent HUNGARY = registerSoundEvent("hungary");
+    public static final RegistryKey<JukeboxSong> HUNGARY_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "hungary"));
+
+
+    public static final SoundEvent ICELAND = registerSoundEvent("iceland");
+    public static final RegistryKey<JukeboxSong> ICELAND_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "iceland"));
+
+
+    public static final SoundEvent IRELAND = registerSoundEvent("ireland");
+    public static final RegistryKey<JukeboxSong> IRELAND_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "ireland"));
+
+
+    public static final SoundEvent ISLE_OF_MAN = registerSoundEvent("isle_of_man");
+    public static final RegistryKey<JukeboxSong> ISLE_OF_MAN_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "isle_of_man"));
+
+
+    public static final SoundEvent ITALY = registerSoundEvent("italy");
+    public static final RegistryKey<JukeboxSong> ITALY_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "italy"));
+
+
+    public static final SoundEvent JERSEY = registerSoundEvent("jersey");
+    public static final RegistryKey<JukeboxSong> JERSEY_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "jersey"));
+
+
+    public static final SoundEvent LATVIA = registerSoundEvent("latvia");
+    public static final RegistryKey<JukeboxSong> LATVIA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "latvia"));
+
+
+    public static final SoundEvent LITHUANIA = registerSoundEvent("lithuania");
+    public static final RegistryKey<JukeboxSong> LITHUANIA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "lithuania"));
+
+
+    public static final SoundEvent LUXEMBOURG = registerSoundEvent("luxembourg");
+    public static final RegistryKey<JukeboxSong> LUXEMBOURG_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "luxembourg"));
+
+
+    public static final SoundEvent MACEDONIA = registerSoundEvent("macedonia");
+    public static final RegistryKey<JukeboxSong> MACEDONIA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "macedonia"));
+
+
+    public static final SoundEvent MALTA = registerSoundEvent("malta");
+    public static final RegistryKey<JukeboxSong> MALTA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "malta"));
+
+
+    public static final SoundEvent MOLDOVA = registerSoundEvent("moldova");
+    public static final RegistryKey<JukeboxSong> MOLDOVA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "moldova"));
+
+
+    public static final SoundEvent MONACO = registerSoundEvent("monaco");
+    public static final RegistryKey<JukeboxSong> MONACO_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "monaco"));
+
+
+    public static final SoundEvent MONTENEGRO = registerSoundEvent("montenegro");
+    public static final RegistryKey<JukeboxSong> MONTENEGRO_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "montenegro"));
+
+
+    public static final SoundEvent NETHERLANDS = registerSoundEvent("netherlands");
+    public static final RegistryKey<JukeboxSong> NETHERLANDS_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "netherlands"));
+
+
+    public static final SoundEvent NORWAY = registerSoundEvent("norway");
+    public static final RegistryKey<JukeboxSong> NORWAY_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "norway"));
+
+
+    public static final SoundEvent POLAND = registerSoundEvent("poland");
+    public static final RegistryKey<JukeboxSong> POLAND_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "poland"));
+
+
+    public static final SoundEvent PORTUGAL = registerSoundEvent("portugal");
+    public static final RegistryKey<JukeboxSong> PORTUGAL_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "portugal"));
+
+
+    public static final SoundEvent ROMANIA = registerSoundEvent("romania");
+    public static final RegistryKey<JukeboxSong> ROMANIA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "romania"));
+
+
+    public static final SoundEvent RUSSIA = registerSoundEvent("russia");
+    public static final RegistryKey<JukeboxSong> RUSSIA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "russia"));
+
+
+    public static final SoundEvent SAN_MARINO = registerSoundEvent("san_marino");
+    public static final RegistryKey<JukeboxSong> SAN_MARINO_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "san_marino"));
+
+
+    public static final SoundEvent SERBIA = registerSoundEvent("serbia");
+    public static final RegistryKey<JukeboxSong> SERBIA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "serbia"));
+
+
+    public static final SoundEvent SLOVAKIA = registerSoundEvent("slovakia");
+    public static final RegistryKey<JukeboxSong> SLOVAKIA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "slovakia"));
+
+
+    public static final SoundEvent SLOVENIA = registerSoundEvent("slovenia");
+    public static final RegistryKey<JukeboxSong> SLOVENIA_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "slovenia"));
+
+
+    public static final SoundEvent SPAIN = registerSoundEvent("spain");
+    public static final RegistryKey<JukeboxSong> SPAIN_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "spain"));
+
+
+    public static final SoundEvent SWEDEN = registerSoundEvent("sweden");
+    public static final RegistryKey<JukeboxSong> SWEDEN_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "sweden"));
+
+
+    public static final SoundEvent SWITZERLAND = registerSoundEvent("switzerland");
+    public static final RegistryKey<JukeboxSong> SWITZERLAND_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "switzerland"));
+
+
+    public static final SoundEvent TURKEY = registerSoundEvent("turkey");
+    public static final RegistryKey<JukeboxSong> TURKEY_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "turkey"));
+
+
+    public static final SoundEvent UK = registerSoundEvent("uk");
+    public static final RegistryKey<JukeboxSong> UK_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "uk"));
+
+
+    public static final SoundEvent UKRAINE = registerSoundEvent("ukraine");
+    public static final RegistryKey<JukeboxSong> UKRAINE_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "ukraine"));
+
+
+    public static final SoundEvent VATICAN = registerSoundEvent("vatican");
+    public static final RegistryKey<JukeboxSong> VATICAN_KEY =
+        RegistryKey.of(RegistryKeys.JUKEBOX_SONG, Identifier.of(AnthemDiscs.MOD_ID, "vatican"));
+
 
     private static SoundEvent registerSoundEvent(String name) {
         Identifier id = Identifier.of(AnthemDiscs.MOD_ID, name);
